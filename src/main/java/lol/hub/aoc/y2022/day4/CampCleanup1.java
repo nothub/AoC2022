a48c5f315cf4453b784b036eaa6fded6665930d9
hub <code@hub.lol>
2022-12-04 17:54:35 +0100

    day 4 cleanup

--- src/main/java/lol/hub/aoc/y2022/day4/CampCleanup1.java
+++ src/main/java/lol/hub/aoc/y2022/day4/CampCleanup1.java
@@ -4,16 +4,21 @@ import lol.hub.aoc.Solver;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class CampCleanup1 implements Solver<List<String>, Integer> {
-    @Override
-    public Integer solve(List<String> input) {
+    static int analyze(List<String> input, Predicate<int[]> predicate) {
         return (int) input.stream()
             .map(line -> line.split("[,-]"))
             .map(arr -> Arrays.stream(arr)
                 .mapToInt(Integer::parseInt)
                 .toArray())
-            .filter(a -> a[0] >= a[2] && a[1] <= a[3] || a[0] <= a[2] && a[1] >= a[3])
+            .filter(predicate)
             .count();
+    }
+
+    @Override
+    public Integer solve(List<String> input) {
+        return analyze(input, a -> a[0] >= a[2] && a[1] <= a[3] || a[0] <= a[2] && a[1] >= a[3]);
     }
 }
--- src/main/java/lol/hub/aoc/y2022/day4/CampCleanup2.java
+++ src/main/java/lol/hub/aoc/y2022/day4/CampCleanup2.java
@@ -2,18 +2,13 @@ package lol.hub.aoc.y2022.day4;
 
 import lol.hub.aoc.Solver;
 
-import java.util.Arrays;
 import java.util.List;
+
+import static lol.hub.aoc.y2022.day4.CampCleanup1.analyze;
 
 public class CampCleanup2 implements Solver<List<String>, Integer> {
     @Override
     public Integer solve(List<String> input) {
-        return (int) input.stream()
-            .map(line -> line.split("[,-]"))
-            .map(arr -> Arrays.stream(arr)
-                .mapToInt(Integer::parseInt)
-                .toArray())
-            .filter(a -> a[1] >= a[2] && a[0] <= a[3])
-            .count();
+        return analyze(input, a -> a[1] >= a[2] && a[0] <= a[3]);
     }
 }
